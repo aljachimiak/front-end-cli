aef1d0f0916e2d4c570e434ab91b77c0a9f84a12
Al Jachimiak <aljachimiak@gmail.com>
2016-05-01 18:13:22 -0400

    react presentation notes and cleaning up

--- front-end/main.jsx
+++ front-end/main.jsx
@@ -4,6 +4,14 @@ var ReactDOM = require('react-dom');
 var PubSub = require('pubsub-js');
 var Sections = ['intro', 'git', 'vagrant', 'nodejs', 'sass', 'react', 'conclusion'];
 
+//
+// Let's be super clear here:
+// Using two unrelated components
+// and firing off an event
+// is not the 'react' way of doing this.
+// I didn't want to have the entire page renered as a React app :-)
+//
+
 var Control = React.createClass({
   onclick: function() {
     // when a product is clicked on, we publish a message on the topic 'products' and we pass the product name
